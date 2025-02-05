f388bf178a53217fef54451e0d22943813401853
sirajshaikh-dev <ss1425306@gmail.com>
2025-02-05 20:08:35 +0530

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import {Route,Routes} from 'react-router-dom'
+import Home from './components/Home'
+
+function App() {
+
+  return (
+    <div className='bg-[#1f1e24] w-screen h-screen'>
+     <Routes>
+        <Route path='/' element={<Home/>} />
+     </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Home() {
+  document.title= 'Homepage'
+  return (
+    <div className='w-[20%] h-full'>Home</div>
+  )
+}
+
+export default Home
